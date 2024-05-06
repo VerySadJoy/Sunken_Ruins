82955c3e8aff49431593fe849a992eb7f129f084
Joy <jinha497@gmail.com>
2024-05-07 00:20:59 +0900

    Add Camera Movement(Cinemachine)
    카메라 추가했음

--- SunkenRuins/Assets/Script/PlayerManager.cs
+++ SunkenRuins/Assets/Script/PlayerManager.cs
@@ -10,8 +10,16 @@ namespace SunkenRuins {
         [SerializeField] private float moveAcceleration = 30f;
         [SerializeField] private float moveDecceleration = 50f;
 
+        [Header("Follow Camera Target")]
+        // Cinemachine Virtual Camera의 Follow로 등록된 오브젝트를 지정해줘야 함!
+        // 새로운 높이의 플랫폼에 착지하기 전까지 카메라의 y축 좌표를 일정하게 유지하는 용도.
+        [SerializeField] private GameObject cameraFollowTarget;
+        // 바라보는 방향으로 얼마나 앞에 있는 지점을 카메라가 추적할 것인지
+        [SerializeField, Range(0f, 2f)] private float cameraLookAheadDistance = 1f;
+
         //Component
         private Rigidbody2D rb;
+        private SpriteRenderer spriteRenderer;
 
         private PlayerControl playerControl; // Input System
         private bool isFacingRight = true;
@@ -21,6 +29,7 @@ namespace SunkenRuins {
             playerControl.Player.Enable();
 
             rb = GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         public void SetInputEnabled(bool enable) { // 컷신이나 뭐할때 Input 죽이는 용
@@ -46,16 +55,20 @@ namespace SunkenRuins {
             float updatedVelocityX = Mathf.MoveTowards(rb.velocity.x, desiredVelocityX, accelerationX * Time.deltaTime);
             float updatedVelocityY = Mathf.MoveTowards(rb.velocity.y, desiredVelocityY, accelerationY * Time.deltaTime);
             rb.velocity = new Vector2(updatedVelocityX, updatedVelocityY);
+            UpdateFacingDirection(moveInput.x);
         }
 
         private void FixedUpdate() {
             HandleMoveInput();
+
+            UpdateCameraFollowTarget();
         }
 
-        private void UpdateFacingDirection(float moveInput) {
-            if (moveInput != 0f) {
-                isFacingRight = moveInput > 0f;
+        private void UpdateFacingDirection(float moveInputX) {
+            if (moveInputX != 0f) {
+                isFacingRight = moveInputX > 0f;
             }
+            spriteRenderer.flipX = !isFacingRight;
         }
 
         private float ChooseAcceleration(float moveInput, float desiredVelocityX) {
@@ -72,6 +85,15 @@ namespace SunkenRuins {
             }
             // Case 3) 기존 방향으로 계속 이동하는 경우
             return moveAcceleration;
+        }
+
+        private void UpdateCameraFollowTarget() {
+            Vector2 newPosition = transform.position;
+
+            // 바라보는 방향으로 look ahead
+            //newPosition.x += isFacingRight ? cameraLookAheadDistance : -cameraLookAheadDistance;
+            Debug.Log(newPosition);
+            cameraFollowTarget.transform.position = newPosition;
         }
     }
 }
