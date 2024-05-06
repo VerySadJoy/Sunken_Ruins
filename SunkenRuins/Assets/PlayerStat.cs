c68416470e64ab417a442d97776aac19fdfa55b5
Joy <jinha497@gmail.com>
2024-05-07 01:09:30 +0900

    Implement HP System
    체력 시스템 구현, 감소배율 등 추후 조정필요

---
+++ SunkenRuins/Assets/PlayerStat.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SunkenRuins {
+    public class PlayerStat : MonoBehaviour {
+        [Header("Stat")]
+        public int playerMaxHealth;
+        public int playerCurrentHealth;
+        public float playerMaxEnergy;
+        public float playerCurrentEnergy;
+        public int healthDecreaseRate;
+
+        [Header("Movement")]
+        public float initialMoveSpeed = 5f; //부스트 미사용 최고 이동속도
+        public float boostMoveSpeed;
+        public float turnAcceleration = 60f;
+        public float moveAcceleration = 30f;
+        public float moveDecceleration = 50f;
+
+        private void Start() {
+            playerCurrentHealth = playerMaxHealth;
+            playerCurrentEnergy = playerMaxEnergy;
+            StartCoroutine(DecreaseHealthOverTime());
+        }
+
+        private System.Collections.IEnumerator DecreaseHealthOverTime() {
+            while (playerCurrentHealth > 0) {
+                yield return new WaitForSeconds(1f);
+                playerCurrentHealth -= healthDecreaseRate;
+            }
+            Debug.Log("끼엑 사망");
+        }
+    }
+
+    
+}
+
+
--- SunkenRuins/Assets/Script/PlayerManager.cs
+++ SunkenRuins/Assets/Script/PlayerManager.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 namespace SunkenRuins {
     public class PlayerManager : MonoBehaviour {
-        [Header("Move")]
-        [SerializeField] private float maxMoveSpeed = 1.5f; // 이동속도
-        [SerializeField] private float turnAcceleration = 60f;
-        [SerializeField] private float moveAcceleration = 30f;
-        [SerializeField] private float moveDecceleration = 50f;
-
         [Header("Follow Camera Target")]
         // Cinemachine Virtual Camera의 Follow로 등록된 오브젝트를 지정해줘야 함!
         // 새로운 높이의 플랫폼에 착지하기 전까지 카메라의 y축 좌표를 일정하게 유지하는 용도.
@@ -20,6 +14,7 @@ namespace SunkenRuins {
         //Component
         private Rigidbody2D rb;
         private SpriteRenderer spriteRenderer;
+        private PlayerStat playerStat;
 
         private PlayerControl playerControl; // Input System
         private bool isFacingRight = true;
@@ -30,6 +25,7 @@ namespace SunkenRuins {
 
             rb = GetComponent<Rigidbody2D>();
             spriteRenderer = GetComponent<SpriteRenderer>();
+            playerStat = GetComponent<PlayerStat>();
         }
 
         public void SetInputEnabled(bool enable) { // 컷신이나 뭐할때 Input 죽이는 용
@@ -44,8 +40,8 @@ namespace SunkenRuins {
         private void HandleMoveInput() {
             Vector2 moveInput = playerControl.Player.Move.ReadValue<Vector2>();
             // 원하는 속도를 계산
-            float desiredVelocityX = maxMoveSpeed * moveInput.x * maxMoveSpeed;
-            float desiredVelocityY = maxMoveSpeed * moveInput.y * maxMoveSpeed;
+            float desiredVelocityX = playerStat.initialMoveSpeed * moveInput.x;
+            float desiredVelocityY = playerStat.initialMoveSpeed * moveInput.y;
 
             // 방향 전환 여부에 따라 다른 가속도 사용
             float accelerationX = ChooseAcceleration(moveInput.x, desiredVelocityX);
@@ -75,16 +71,16 @@ namespace SunkenRuins {
             // Case 1) 이동을 멈추는 경우
             bool isStopping = moveInput == 0f;
             if (isStopping) {
-                return moveDecceleration;
+                return playerStat.moveDecceleration;
             }
 
             // Case 2) 반대 방향으로 이동하려는 경우
             bool isTurningDirection = rb.velocity.x * desiredVelocityX < 0f;
             if (isTurningDirection) {
-                return turnAcceleration;
+                return playerStat.turnAcceleration;
             }
             // Case 3) 기존 방향으로 계속 이동하는 경우
-            return moveAcceleration;
+            return playerStat.moveAcceleration;
         }
 
         private void UpdateCameraFollowTarget() {
