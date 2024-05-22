45bc51e869cf41f3bfbcbcf1b67cfd66333dec4c
newykang <pretzel0315@gmail.com>
2024-05-22 17:39:56 +0900

    Boost UI Implementation
    - Boost UI Bar
    - Boost Event (BoostBarUI script)
    - PlayerManager Singleton

--- SunkenRuins/Assets/Script/Player/PlayerManager.cs
+++ SunkenRuins/Assets/Script/Player/PlayerManager.cs
@@ -5,8 +5,16 @@ using UnityEngine.InputSystem;
 using Cinemachine;
 using System;
 
-namespace SunkenRuins {
-    public class PlayerManager : MonoBehaviour {
+namespace SunkenRuins
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        // Singleton 구조
+        public static PlayerManager Instance { get; private set; }
+
+        // UnityEvent
+        public event EventHandler OnPlayerBoost;
+
         [Header("Follow Camera Target")]
         [SerializeField] private GameObject cameraFollowTarget;
         // 바라보는 방향으로 얼마나 앞에 있는 지점을 카메라가 추적할 것인지
@@ -33,7 +41,11 @@ namespace SunkenRuins {
         private float boostCooldown = 1f;
         private float boostDuration = 0.5f;
 
-        private void Awake() {
+        private void Awake()
+        {
+            if (Instance != null) Debug.LogError("There is more than one player instance");
+            else Instance = this;
+
             rb = GetComponent<Rigidbody2D>();
             spriteRenderer = GetComponent<SpriteRenderer>();
             playerStat = GetComponent<PlayerStat>();
@@ -41,13 +53,17 @@ namespace SunkenRuins {
             defaultOrthographicSize = virtualCamera.m_Lens.OrthographicSize;
         }
 
-        private void OnTriggerEnter2D(Collider2D other) { // 임시 처리
+        private void OnTriggerEnter2D(Collider2D other)
+        { // 임시 처리
             //소신발언
             //충돌로만 Item 획득이 가능하면 굳이 EventHandler 써야할 이유가 있는지?
-            if (other.gameObject.layer == LayerMask.NameToLayer(itemLayerString)) { //Item 획득
+            if (other.gameObject.layer == LayerMask.NameToLayer(itemLayerString))
+            { //Item 획득
                 ItemSO itemSO = other.gameObject.GetComponent<Item>().GetItemSO();
-                if (itemSO != null) {
-                    switch (itemSO.itemType) {
+                if (itemSO != null)
+                {
+                    switch (itemSO.itemType)
+                    {
                         case ItemType.HealthPotion:
                             playerStat.RestoreHealth(100);
                             break;
@@ -71,43 +87,52 @@ namespace SunkenRuins {
             }
         }
 
-        private void OnEnable() {
+        private void OnEnable()
+        {
             playerControl = new PlayerControl();
             playerControl.Player.Enable();
         }
 
-        private void FixedUpdate() {
+        private void FixedUpdate()
+        {
             HandleMoveInput();
             BoostInput();
             UpdateCameraFollowTarget();
         }
 
-        private void BoostInput() {
+        private void BoostInput()
+        {
             float boostInput = playerControl.Player.Mouse.ReadValue<float>();
-            
-            if (boostInput > 0 && playerStat.playerCurrentEnergy > 0 && !isBoosting && Time.time > lastBoostTime + boostCooldown) {
+
+            if (boostInput > 0 && playerStat.playerCurrentEnergy > 0 && !isBoosting && Time.time > lastBoostTime + boostCooldown)
+            {
                 PrepareBoost();
             }
-            if (boostInput == 0 && isBoostPreparing) {
+            if (boostInput == 0 && isBoostPreparing)
+            {
                 ExecuteBoost();
             }
-            if (Input.GetKeyDown(KeyCode.E) && isBoostPreparing) { //이거 나중에 Input System으로 수정해야함
+            if (Input.GetKeyDown(KeyCode.E) && isBoostPreparing)
+            { //이거 나중에 Input System으로 수정해야함
                 CancelBoost();
             }
-            
+
         }
 
-        private void PrepareBoost() {
+        private void PrepareBoost()
+        {
             isBoostPreparing = true;
             Time.timeScale = 0.5f;
-
-            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, defaultOrthographicSize, zoomSpeed * Time.deltaTime);; //Zoom In
+            OnPlayerBoost?.Invoke(this, EventArgs.Empty); // 플레이어가 부스트를 시도하는 것을 UI에 알림
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, defaultOrthographicSize, zoomSpeed * Time.deltaTime); ; //Zoom In
             //TODO:
             // UI 보이기
             Debug.Log("준비");
         }
 
-        private void ExecuteBoost() {
+        private void ExecuteBoost()
+        {
+            Time.timeScale = 1f;
             isBoostPreparing = false;
             isBoosting = true;
             lastBoostTime = Time.time;
@@ -120,9 +145,11 @@ namespace SunkenRuins {
             StartCoroutine(BoostMovement(boostDirection, boostSpeed));
         }
 
-        private IEnumerator BoostMovement(Vector2 direction, float speed) {
+        private IEnumerator BoostMovement(Vector2 direction, float speed)
+        {
             float elapsed = 0f;
-            while (elapsed < boostDuration) {
+            while (elapsed < boostDuration)
+            {
                 float moveStep = speed * Time.deltaTime;
                 transform.Translate(direction * moveStep, Space.World);
                 elapsed += Time.deltaTime;
@@ -143,7 +170,8 @@ namespace SunkenRuins {
             isBoosting = false;
         }
 
-        private void CancelBoost() {
+        private void CancelBoost()
+        {
             isBoostPreparing = false;
             Time.timeScale = 1f;
 
@@ -153,16 +181,20 @@ namespace SunkenRuins {
             Debug.Log("취소");
         }
 
-        public void SetInputEnabled(bool enable) { // 컷신이나 뭐할때 Input 죽이는 용
-            if (enable) {
+        public void SetInputEnabled(bool enable)
+        { // 컷신이나 뭐할때 Input 죽이는 용
+            if (enable)
+            {
                 playerControl.Player.Enable();
             }
-            else {
+            else
+            {
                 playerControl.Player.Disable();
             }
         }
 
-        private void HandleMoveInput() {
+        private void HandleMoveInput()
+        {
             Vector2 moveInput = playerControl.Player.Move.ReadValue<Vector2>();
             // 원하는 속도를 계산
             float desiredVelocityX = playerStat.initialMoveSpeed * moveInput.x;
@@ -180,30 +212,36 @@ namespace SunkenRuins {
             UpdateFacingDirection(moveInput.x);
         }
 
-        private void UpdateFacingDirection(float moveInputX) {
-            if (moveInputX != 0f) {
+        private void UpdateFacingDirection(float moveInputX)
+        {
+            if (moveInputX != 0f)
+            {
                 isFacingRight = moveInputX > 0f;
             }
             spriteRenderer.flipX = !isFacingRight;
         }
 
-        private float ChooseAcceleration(float moveInput, float desiredVelocityX) {
+        private float ChooseAcceleration(float moveInput, float desiredVelocityX)
+        {
             // Case 1) 이동을 멈추는 경우
             bool isStopping = moveInput == 0f;
-            if (isStopping) {
+            if (isStopping)
+            {
                 return playerStat.moveDecceleration;
             }
 
             // Case 2) 반대 방향으로 이동하려는 경우
             bool isTurningDirection = rb.velocity.x * desiredVelocityX < 0f;
-            if (isTurningDirection) {
+            if (isTurningDirection)
+            {
                 return playerStat.turnAcceleration;
             }
             // Case 3) 기존 방향으로 계속 이동하는 경우
             return playerStat.moveAcceleration;
         }
 
-        private void UpdateCameraFollowTarget() {
+        private void UpdateCameraFollowTarget()
+        {
             Vector2 newPosition = transform.position;
 
             // 바라보는 방향으로 look ahead
@@ -229,4 +267,3 @@ namespace SunkenRuins {
         // }
     }
 }
-
