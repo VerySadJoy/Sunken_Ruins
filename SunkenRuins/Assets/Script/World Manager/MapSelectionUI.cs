40f6686dc82124d9e13dd765c31611466f6f2448
Joy Seo <115052493+VerySadJoy@users.noreply.github.com>
2024-07-10 17:00:06 +0900

    Implement Movement Functionality
    이동

--- SunkenRuins/Assets/Script/World Manager/MapSelectionUI.cs
+++ SunkenRuins/Assets/Script/World Manager/MapSelectionUI.cs
@@ -9,15 +9,18 @@ namespace SunkenRuins {
 
         public MapUI[] MapUIList;
         public MapAsset[] MapAssetList;
-
-        private void Awake() {
-            
-        }
         
         private void Start() {
             MapUIList[0].mapNameText.text = MapAssetList[0].mapName;
             MapUIList[1].mapNameText.text = MapAssetList[1].mapName;
             MapUIList[2].mapNameText.text = MapAssetList[2].mapName;
+        }
+
+        public void LeftBoxOnClick(){
+            MapUIList[0].transform.DOMoveX(150, 1);
+            MapUIList[0].transform.DOScale(0.5f, 1);
+            MapUIList[1].transform.DOMoveX(450, 1);
+            MapUIList[1].transform.DOScale(2f, 1);
         }
     }
 }
