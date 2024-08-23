be2374c180bf9b5a6e5706b588a48cacf5c8dcf5
newykang <pretzel0315@gmail.com>
2024-08-23 20:33:52 +0900

    Update EnergyBarUI

--- SunkenRuins/Assets/EnergyBarUI.cs
+++ SunkenRuins/Assets/EnergyBarUI.cs
@@ -1,39 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace SunkenRuins {
     public class EnergyBarUI : MonoBehaviour
     {
         private PlayerManager player;
         private Image[] energyLevel;
+        [SerializeField] private Sprite energyFullSprite;
+        [SerializeField] private Sprite energyBlankSprite;
 
         private void Awake () {
             player = GetComponentInParent<PlayerManager>();
             energyLevel = GetComponentsInChildren<Image>();
         }
         private void Update() {
-            if (player.playerStat.playerCurrentEnergy == 3) {
-                energyLevel[0].color = Color.green;
-                energyLevel[1].color = Color.green;
-                energyLevel[2].color = Color.green;
-            }
-            else if (player.playerStat.playerCurrentEnergy == 2) {
-                energyLevel[0].color = Color.green;
-                energyLevel[1].color = Color.white;
-                energyLevel[2].color = Color.green;
-            }
-            else if (player.playerStat.playerCurrentEnergy == 1) {
-                energyLevel[0].color = Color.white;
-                energyLevel[1].color = Color.white;
-                energyLevel[2].color = Color.green;
-            }
-            else if (player.playerStat.playerCurrentEnergy == 0) {
-                energyLevel[0].color = Color.white;
-                energyLevel[1].color = Color.white;
-                energyLevel[2].color = Color.white;
-            }
+            // if (player.playerStat.playerCurrentEnergy >= 3) {
+            //     energyLevel[0].image = energyFullSprite;
+            //     energyLevel[1].sprite = energyFullSprite;
+            //     energyLevel[2].sprite = energyFullSprite;
+            // }
+            // else if (player.playerStat.playerCurrentEnergy == 2) {
+            //     energyLevel[0].sprite = energyFullSprite;
+            //     energyLevel[1].sprite = energyFullSprite;
+            //     energyLevel[2].sprite = energyBlankSprite;
+            // }
+            // else if (player.playerStat.playerCurrentEnergy == 1) {
+            //     energyLevel[0].sprite = energyFullSprite;
+            //     energyLevel[1].sprite = energyBlankSprite;
+            //     energyLevel[2].sprite = energyBlankSprite;
+            // }
+            // else if (player.playerStat.playerCurrentEnergy == 0) {
+            //     energyLevel[0].sprite = energyBlankSprite;
+            //     energyLevel[1].sprite = energyBlankSprite;
+            //     energyLevel[2].sprite = energyBlankSprite;
+            // }
         }
         public void SetUIActive(bool isActive)
         {
